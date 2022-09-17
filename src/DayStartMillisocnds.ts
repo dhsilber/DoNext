29145cca4288e7f0fd97d18f8af4d63a223d3f3e
David Silber <ddragontech@gmail.com>
2022-09-17 08:50:32 -0400

    Show routine along with events

--- src/DayStartMillisocnds.ts
+++ src/DayStartMillisocnds.ts
@@ -1,7 +1,6 @@
 import { MinuteMilliseconds } from "./Constants"
 
-export const dayNowStartMilliseconds = () => {
-    const now = new Date()
+const commonDayStart=(now:Date)=>{
     const nowYear = now.getFullYear()
     const nowMonthIndex = now.getMonth()
     const nowMonthDay = now.getDate()
@@ -10,12 +9,12 @@ export const dayNowStartMilliseconds = () => {
     return dayStart
 }
 
+export const dayNowStartMilliseconds = () => {
+    const now = new Date()
+    return commonDayStart(now)
+}
+
 export const dayTimestampStartMilliseconds = (timestamp: number) => {
     const now = new Date(timestamp)
-    const nowYear = now.getFullYear()
-    const nowMonthIndex = now.getMonth()
-    const nowMonthDay = now.getDate()
-    const nowTimeZoneOffset = now.getTimezoneOffset() * MinuteMilliseconds
-    const dayStart = Date.UTC(nowYear, nowMonthIndex, nowMonthDay) + nowTimeZoneOffset
-    return dayStart
+    return commonDayStart(now)
 }
