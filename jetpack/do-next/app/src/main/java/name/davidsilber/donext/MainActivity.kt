1f3f0063e2c737844553ef132dc7e3b52f8f8e30
David Silber <ddragontech@gmail.com>
2023-02-26 17:47:10 -0500

    App prototype should allow to check things off, reset list to undone, and show full list with status

--- jetpack/do-next/app/src/main/java/name/davidsilber/donext/MainActivity.kt
+++ jetpack/do-next/app/src/main/java/name/davidsilber/donext/MainActivity.kt
@@ -1,18 +1,25 @@
 package name.davidsilber.donext
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import name.davidsilber.donext.ui.theme.DoNextTheme
 import java.time.Instant
 
@@ -37,17 +44,45 @@ data class DoItem(val text: String, val date: Instant)
 
 @Composable
 fun DoNextList(doList: List<DoItem>) {
+    var resetCounter by remember { mutableStateOf(1) }
+    var showDoneItems by remember { mutableStateOf(false) }
     Column {
-        Text(text = "Do these things:")
-        LazyColumn {
-            items(doList) { item -> DoNextItem(item) }
+        Text(text = "Do these things:\n")
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            items(doList) { item -> DoNextItem(item, resetCounter, showDoneItems) }
+        }
+        Row {
+            Button(onClick = { resetCounter += 1 }) {
+
+            }
+            Button(onClick = { showDoneItems = !showDoneItems }) {
+
+            }
         }
     }
 }
 
 @Composable
-fun DoNextItem(item: DoItem) {
-    Text(text = item.text)
+fun DoNextItem(item: DoItem, resetCounter: Int, showDoneItems: Boolean) {
+    var isHidden by remember(key1 = resetCounter) { mutableStateOf(false) }
+    Row(modifier = Modifier
+        .clickable { isHidden = !isHidden }
+    ) {
+        if (!isHidden) {
+            Text(
+                text = item.text,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+        if (isHidden && showDoneItems) {
+            Text(
+                text = item.text,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .alpha(0.5f)
+            )
+        }
+    }
 }
 
 val previewData = listOf(
@@ -56,7 +91,10 @@ val previewData = listOf(
     DoItem("Tweak gizmo parameters", Instant.EPOCH),
 )
 
-@Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+)
 @Composable
 fun DefaultPreview() {
     DoNextTheme {
