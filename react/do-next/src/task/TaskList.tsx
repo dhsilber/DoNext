cc1d9a690ec1b778e91e0399eb64e811c01711e8
David Silber <ddragontech@gmail.com>
2023-11-25 21:34:58 -0500

    Keystrokes to move between tasks

--- react/do-next/src/task/TaskList.tsx
+++ react/do-next/src/task/TaskList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useInsertionEffect, useReducer } from 'react'
 import { Task, TaskSet } from '../DoData'
 import TaskListElement from './TaskListElement'
 
@@ -8,7 +8,58 @@ interface TaskListProps {
     setEditTask: (task: Task) => void
 }
 
+type State = {
+    taskLine: number
+}
+
+type Action =
+| { type: 'down' }
+| { type: 'up' }
+
 const TaskList = ({ taskSet, save, setEditTask }: TaskListProps) => {
+    const [cursor, dispatch] = useReducer(keystrokeReducer, {taskLine: 0});
+
+    function keystrokeReducer(state: State, action: Action): State {
+        switch (action.type) {
+            case 'down':
+                return { taskLine: state.taskLine + 1 }
+            case 'up':
+                return { taskLine: state.taskLine - 1 }
+        }
+    }
+
+    function handleMoveDown() {
+        dispatch({
+            type: 'down'
+        })
+    }
+
+    function handleMoveUp() {
+        dispatch({
+            type: 'up'
+        })
+    }
+
+    useInsertionEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+          switch(event.key) {
+              case 'j':
+                handleMoveDown()
+                break
+            case 'k':
+                handleMoveUp()
+                break
+           }
+ 
+        }
+    
+        document.addEventListener("keydown", handleKeyDown)
+    
+        return function cleanup() {
+          document.removeEventListener("keydown", handleKeyDown)
+        }
+    }, [])
+
     return <>
         {taskSet.tasks
             .filter(task => task.archived === 0)
@@ -16,7 +67,7 @@ const TaskList = ({ taskSet, save, setEditTask }: TaskListProps) => {
                 return <TaskListElement
                     key={'task' + task.id}
                     task={task}
-                    currentTask = {index == 0}
+                    currentTask = {index == cursor.taskLine}
                     save={save}
                     setEditTask={setEditTask}
                 />
