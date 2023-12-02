4386fe9094113b33f491b5d146604f165328e58b
David Silber <ddragontech@gmail.com>
2023-12-02 08:40:11 -0500

    Navigation to and from Data Interchange page should exist

---
+++ react/do-next/src/DataInterchange.tsx
@@ -0,0 +1,9 @@
+import './DoNext.css'
+
+function DataInterchange() {
+  return <>
+    <a className="btn" href="/">Return to main page</a>
+  </>
+}
+
+export default DataInterchange
--- react/do-next/src/Edit.tsx
+++ react/do-next/src/Edit.tsx
@@ -1,15 +1,4 @@
-import { useEffect, useState } from 'react'
 import './DoNext.css'
-import RepeatingDos from './todo/RepeatingDos'
-import useLocalStorageState from 'use-local-storage-state'
-import Events from './event/Events'
-import { defaultToDoData } from './storage/Storage'
-import Storage from './storage/Storage'
-import { MinuteMilliseconds, TodoStorageKey } from './Constants'
-import Projects from './project/Projects'
-import Tracks from './track/Tracks'
-import Tasks from './task/Tasks'
-import Analyze from './analyze/Analyze'
 
 function Edit() {
   return <>
